a13ee5f051210620763056a14bc61fa9775737a0
HydarAlee <alibabaa1991@gmail.com>
2025-07-31 00:12:05 +0500

    Server is in running condition

--- server/db.ts
+++ server/db.ts
@@ -2,16 +2,18 @@ import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/simple-schema";
-
+import 'dotenv/config';
 neonConfig.webSocketConstructor = ws;
 
+
+console.log("process.env.DATABASE_URL", process.env.DATABASE_URL)
 if (!process.env.DATABASE_URL) {
   throw new Error(
     "DATABASE_URL must be set. Did you forget to provision a database?",
   );
 }
 
-export const pool = new Pool({ 
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 20,
   idleTimeoutMillis: 30000,
